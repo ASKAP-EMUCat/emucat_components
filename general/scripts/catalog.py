0ed048e5e3c796a386a44d700bf28cb3a00c6731
davepallot <dave.pallot@uwa.edu.au>
2020-09-18 15:53:43 +0800

    Moved

---
+++ general/scripts/catalog.py
@@ -0,0 +1,220 @@
+#!python3
+import xml.etree.ElementTree as ET
+import aiofiles
+import asyncio
+import asyncpg
+import csv
+import sys
+import random
+import string
+import logging
+import argparse
+import configparser
+
+
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s')
+
+
+async def db_moasic_upsert(conn, row):
+    mosaic_id = await conn.fetchrow('INSERT INTO emucat.mosaics ("ser_id", "table_version", "image_file", '
+                                     '"flag_subsection",'
+                                     '"subsection", "flag_statsec", "statsec", "search_type", "flag_negative", '
+                                     '"flag_baseline", "flag_robuststats", "flag_fdr", "threshold", "flag_growth", '
+                                     '"growth_threshold", "min_pix", "min_channels", "min_voxels", "flag_adjacent", '
+                                     '"thresh_velocity", "flag_rejectbeforemerge", "flag_twostagemerging", '
+                                     '"pixel_centre", "flag_smooth", "flag_atrous", "reference_frequency", '
+                                     '"threshold_actual") '
+                                     'VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,'
+                                     '$16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27) '
+                                     'ON CONFLICT ("ser_id", "subsection") '
+                                     'DO UPDATE SET subsection = EXCLUDED.subsection RETURNING id',
+                                     *row)
+    return mosaic_id[0]
+
+
+async def db_components_upsert_many(conn, rows):
+    await conn.executemany('INSERT INTO emucat.components ("mosaic_id","island_id","component_id",'
+                                     '"component_name","ra_hms_cont","dec_hms_cont","ra_deg_cont",'
+                                     '"dec_deg_cont","ra_err","dec_err","freq","flux_peak","flux_peak_err",'
+                                     '"flux_int","flux_int_err","maj_axis","min_axis","pos_ang","maj_axis_err",'
+                                     '"min_axis_err","pos_ang_err","maj_axis_deconv","min_axis_deconv",'
+                                     '"pos_ang_deconv","maj_axis_deconv_err","min_axis_deconv_err",'
+                                     '"pos_ang_deconv_err","chi_squared_fit","rms_fit_gauss","spectral_index",'
+                                     '"spectral_curvature","spectral_index_err","spectral_curvature_err",'
+                                     '"rms_image","has_siblings","fit_is_estimate","spectral_index_from_tt",'
+                                     '"flag_c4","comment")'
+                                     'VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15,'
+                                     '$16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29,'
+                                     '$30, $31, $32, $33, $34, $35, $36, $37, $38, $39) '
+                                     'ON CONFLICT ("mosaic_id", "component_name", "ra_deg_cont", "dec_deg_cont") DO NOTHING',
+                                     rows)
+
+
+async def _get_file_bytes(path: str, mode: str = 'rb'):
+    buffer = []
+
+    async with aiofiles.open(path, mode) as f:
+        while True:
+            buff = await f.read()
+            if not buff:
+                break
+            buffer.append(buff)
+        if 'b' in mode:
+            return b''.join(buffer)
+        else:
+            return ''.join(buffer)
+
+
+def convert(value, datatype):
+    if datatype == 'float':
+        return float(value)
+    elif datatype =='boolean':
+        return bool(value)
+    elif datatype == 'int':
+        return int(value)
+    elif datatype == 'double':
+        return float(value)
+    return value
+
+
+async def import_selavy_catalog(conn, ser_name: str, filename: str):
+    ser_id = await conn.fetchrow('SELECT id from emucat.source_extraction_regions where name=$1', ser_name)
+    if not ser_id:
+        raise Exception('Source extraction region not found')
+
+    ser_id = ser_id[0]
+
+    ns = {'ivoa': 'http://www.ivoa.net/xml/VOTable/v1.3'}
+
+    content = await _get_file_bytes(filename, mode='r')
+    root = ET.fromstring(content)
+
+    mosaic_map = {'ser_id': None,
+                'table_version': None,
+                'imageFile': None,
+                'flagSubsection': False,
+                'subsection': None,
+                'flagStatSec': False,
+                'StatSec': None,
+                'searchType': None,
+                'flagNegative': False,
+                'flagBaseline': False,
+                'flagRobustStats': False,
+                'flagFDR': False,
+                'threshold': None,
+                'flagGrowth': False,
+                'growthThreshold': 0.0,
+                'minPix': 0,
+                'minChannels': 0,
+                'minVoxels': 0,
+                'flagAdjacent': False,
+                'threshVelocity': None,
+                'flagRejectBeforeMerge': False,
+                'flagTwoStageMerging': False,
+                'pixelCentre': None,
+                'flagSmooth': False,
+                'flagATrous': False,
+                'Reference frequency': 0.0,
+                'thresholdActual': 0.0}
+
+    for param in root.findall('./ivoa:RESOURCE/ivoa:TABLE/ivoa:PARAM', ns):
+        key = param.get('name')
+        value = convert(param.get('value'), param.get('datatype'))
+        mosaic_map[key] = value
+
+    mosaic_map['ser_id'] = ser_id
+    params = list(mosaic_map.values())
+    mosaic_id = await db_moasic_upsert(conn, params)
+
+    datatypes = []
+    for field in root.findall('./ivoa:RESOURCE/ivoa:TABLE/ivoa:FIELD', ns):
+        datatypes.append(field.get('datatype'))
+
+    rows = []
+    for i, tr in enumerate(root.findall('./ivoa:RESOURCE/ivoa:TABLE/ivoa:DATA/ivoa:TABLEDATA/ivoa:TR', ns)):
+        cat = [convert(td.text.strip(), datatypes[j]) for j, td in enumerate(tr)]
+        cat.insert(0, mosaic_id)
+        rows.append(cat)
+
+    await db_components_upsert_many(conn, rows)
+
+
+def import_selavy(args):
+    asyncio.run(import_selavy_votable(args.ser, args.input, args.credentials))
+
+
+async def import_selavy_votable(ser_name: str, filename: str, credentials: str):
+    config = configparser.ConfigParser()
+    config.read(credentials)
+    user = config['emucat_database']['user']
+    password = config['emucat_database']['password']
+    database = config['emucat_database']['database']
+    host = config['emucat_database']['host']
+
+    conn = await asyncpg.connect(user=user, password=password, database=database, host=host)
+    await import_selavy_catalog(conn, ser_name, filename)
+
+
+def parse_allwise_votable(filename: str):
+    ns = 'http://www.ivoa.net/xml/VOTable/v1.3'
+    ET.register_namespace("ivoa", ns)
+    q = ET.QName(ns, "TR")
+    context = ET.iterparse(filename, events=("start", "end"))
+    context = iter(context)
+    ev, root = next(context)
+
+    for ev, el in context:
+        if ev == 'end' and el.tag == q:
+            data = [el[0].text, f'({el[1].text}d, {el[2].text}d)']
+            yield data
+            root.clear()
+
+
+async def export_allwise_votable_to_csv(input_path: str, output_path: str):
+    with open(output_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        for data in parse_wise_votable(input_path):
+            writer.writerows([data])
+
+
+def random_word(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
+
+async def import_allwise_catalog_from_csv(input_path: str):
+    conn = await asyncpg.connect(user='admin', password='admin', database='emucat', host='localhost')
+    async with conn.transaction():
+        with open(input_path, 'rb') as f:
+            table_name = random_word(6)
+            temp_sql = f'CREATE TEMP TABLE {table_name} ON COMMIT DROP AS SELECT * FROM emucat.allwise WITH NO DATA;'
+            await conn.fetchrow(temp_sql)
+            copy_result = await conn.copy_to_table(table_name=table_name, source=f, format='csv')
+            print(copy_result)
+            copy_sql = f'INSERT INTO emucat.allwise SELECT * FROM {table_name} ORDER BY (designation) ' \
+                       f'ON CONFLICT (designation) DO NOTHING'
+            await conn.fetchrow(copy_sql)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='EMUCat', description='EMUCat catalog functions.')
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    input_selavy = subparsers.add_parser('import_selavy', help='Import selavy component catalog into EMUCat.')
+    input_selavy.add_argument('-s', '--ser', help='Source extraction region.', type=str, required=True)
+    input_selavy.add_argument('-i', '--input', help='Selavy votable.', type=str, required=True)
+    input_selavy.add_argument('-c', '--credentials', help='Credentials file.', required=True)
+    input_selavy.set_defaults(func=import_selavy)
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+        exit(0)
+    except Exception as e:
+        logging.exception(e)
+        exit(1)
---
+++ likelihood_ratio/likelihood_ratio/lr_wrapper_emucat.py
@@ -0,0 +1,195 @@
+#!python3
+
+###wrapper for EMUcat likelihood ratio code
+###takes VOTable EMUcat files
+###converts to work with LoTSS LR code
+###runs LR
+###converts output back to VOTable for EMUcat
+
+
+import os
+import argparse, subprocess, os, numpy as np, pandas as pd
+from astropy.table import Table
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+from likelihood_ratio_matching import main
+
+###########################################################################
+###def functions
+
+def fake_mask(catfile, catformat='votable', acol='ra', dcol='dec',
+              outfile='fakemask.fits', overwrite=True):
+    
+    'fake a mask image (all unmasked) to enable LR code to run using just catalogue info'
+    'use AllWISE image parameters, set array values to 1'
+    'in lieu of dowloading AllWISE images and mask on the fly'
+    'make npx dependent on ra/dec range covered, will work but slow for large regions'
+    
+    ###obtain ref ra/dec for fake mask from catalogue data
+    catdata = Table.read(catfile, format=catformat)
+
+    ra, dec = np.array(catdata[acol]), np.array(catdata[dcol])
+    
+    ##dec easy, ra needs to account for ra wrapping at 360/0deg
+    amin, amax = np.min(ra), np.max(ra)
+    dmin, dmax = np.min(dec), np.max(dec)
+    
+    acen = amin + ((amax-amin)/2)
+    dcen = dmin + ((dmax-dmin)/2)
+    
+    ###account for ra wrapping at 360 - n.b. will break if cone search radius is >90deg!!!
+    if amax - amin > 180:
+        acen = (360-amax) + ((amin-(360-amax))/2)
+    
+    ####define number of pixels by wcs world to pix
+    ###use lines defining centre of image to determine size (i.e amin and amax versus dcen)
+    a0d = SkyCoord(ra=amax, dec=dcen, unit='deg')
+    a1d = SkyCoord(ra=amin, dec=dcen, unit='deg')
+    d0a = SkyCoord(ra=acen, dec=dmin, unit='deg')
+    d1a = SkyCoord(ra=acen, dec=dmax, unit='deg')
+
+    dRA, dDec = a0d.separation(a1d), d0a.separation(d1a)
+
+    ###set npx
+    npxa = int((dRA/0.0003819444391411).value)
+    npxd = int((dDec/0.0003819444391411).value)
+
+    ###create data array (4095x4095)
+    unmasked_array = np.zeros((npxd, npxa))
+    
+    ##create fits primary HDU
+    hdu = fits.PrimaryHDU(unmasked_array)
+    
+    ###adjust header to privide necessary wcs information
+    hdu.header['CTYPE1'] = 'RA---SIN'
+    hdu.header['CTYPE2'] = 'DEC--SIN'
+    hdu.header['CRVAL1'] = acen
+    hdu.header['CRVAL2'] = dcen
+    hdu.header['CRPIX1'] = npxa/2
+    hdu.header['CRPIX2'] = npxd/2
+    hdu.header['CDELT1'] = -0.0003819444391411
+    hdu.header['CDELT2'] = 0.0003819444391411
+    hdu.header['CUNIT1'] = 'deg'
+    hdu.header['CUNIT2'] = 'deg'
+    
+    ###convert to HDU list and write to file
+    hdul = fits.HDUList(hdu)
+    
+    hdul.writeto(outfile, overwrite=overwrite)
+    return
+
+
+def add_mask_and_stargal_cols(mwfile, fileformat='fits'):
+    'add Mask and Stargal columns to multiwavelength data'
+    'set all to good for now, update later - stargal can exploit WISE colours'
+    
+    data = Table.read(mwfile, format=fileformat)
+
+    ###mask 0 = good
+    ###sg 1 = good
+    maskarray = np.zeros(len(data))
+    sgarray = np.ones(len(data))
+    
+    data['Mask'] = maskarray
+    data['stargal'] = sgarray
+    
+    data.write(mwfile, overwrite=True)
+    
+    return
+
+
+def VOTable_to_fits(fname_in, fname_out, over_write=True):
+    ###create fits copy of VOTable from EMUcat for use with LR code
+    data = Table.read(fname_in, format='votable')
+    for c in data.columns.values():
+        if c.dtype == 'object':
+            as_str = data[c.name].astype('str')
+            data.replace_column(c.name, as_str)
+
+    data.write(fname_out, format='fits', overwrite=over_write)
+    return
+
+
+def output_to_VOTable(fname_in, fname_out, overwrite=True):
+    data = Table.read(fname_in, format='ascii')
+    data.write(fname_out, format='votable', overwrite=overwrite)
+    return
+
+
+def run_lr(fakefile='fakemask.fits', racol='ra', deccol='dec'):
+    'run wrapped likelihood ratio code'
+    'io format: VOTables'
+    
+    ##parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mwcat', type=str, action='store',
+                        help='multiwavelength data VOTable to convert to fits for LR code')
+    parser.add_argument('--radcat', type=str, action='store',
+                        help='radio data VOTable to convert to fits for LR code')
+    parser.add_argument('--config', type=str, action='store', default='./lr_config.txt',
+                        help='configuration file.')
+    args = parser.parse_args()
+    
+    ###extract info from config file - need outdir
+    config_info = pd.read_table(args.config, sep='\s+').replace("'", "", regex=True)
+    outdir = config_info[(config_info['parameter']=='outdir')].iloc[0]['value']
+
+    try:
+        os.makedirs(outdir)
+    except:
+        pass
+
+    ###create fits tables for mw/radcat
+    table_data = [args.mwcat, args.radcat]
+    fitsnames = [os.path.splitext(fname)[0]+'.fits' for fname in table_data]
+
+    for i in range(len(table_data)):
+        VOTable_to_fits(fname_in=table_data[i], fname_out=fitsnames[i])
+    
+    ###add in mask and stargal columns
+    add_mask_and_stargal_cols(mwfile=fitsnames[0])
+
+    fake_file = os.path.join(outdir, fakefile)
+
+    ###need to place here what to do about mask image, currently assumes present
+    fake_mask(catfile=args.mwcat, acol=racol, dcol=deccol, outfile=fake_file)
+    mask = fake_file
+
+    ###run LR code
+    main(multiwave_cat=fitsnames[0], radio_cat=fitsnames[1], mask_image=mask, config_file=args.config, overwrite=True,
+         snr_cut=5, LR_threshold=0.8)
+
+    ###convert outputs (keep those that end in '_LR_matches.dat') to VOTable
+
+    for filename in os.listdir(outdir):
+        if '_LR_matches.dat' in filename:
+            newname = os.path.join(outdir, filename.split('.')[0] + '.xml')
+            output_to_VOTable(fname_in=os.path.join(outdir, filename), fname_out=newname, overwrite=True)
+
+    ##tidy up - get rid of surplus (add in moving files to target directory)
+    '''for filename in file_list:
+        if filename not in keep_files:
+            os.remove(outdir+filename)
+
+    ###repeat for working directory if outdir != ''
+    if outdir!='':
+        working_directory_list = os.listdir()
+        for filename in working_directory_list:
+            if filename not in keep_files:
+                os.remove(filename)'''
+
+    return
+
+###########################################################################
+
+if __name__ == "__main__":
+    run_lr()
+
+###to do:
+##1) upgrade to grab and mosaic WISE images
+##2) test with multi-band data
+##3) improve stargal and mask flagging in mwcat
+
+
+
